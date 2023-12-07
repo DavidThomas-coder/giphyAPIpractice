9a0e049d3d6095446d504fafdc8364d0b2d99eea
RipNasty <det625@gmail.com>
2023-12-07 17:24:46 -0500

    api call is working

--- apiCall.js
+++ apiCall.js
@@ -1,3 +1,17 @@
-import got from "got";
+import got from 'got';
 
-const apiKey = 'YgXvristQ3xbvJPknOjl9Sl53cZdX68A'
+const apiKey = 'YgXvristQ3xbvJPknOjl9Sl53cZdX68A';
+const apiUrl = 'https://api.giphy.com/v1/gifs/trending?api_key=YJX6Vs4kayAeOM4etN7P5ueL4ie4wU5D';
+
+async function getGiphy() {
+    try {
+        const response = await got(apiUrl);
+        const data = JSON.parse(response.body);
+
+        console.log(data);
+    } catch (error) {
+        console.error('Error in fetch', error.response.body);
+    }
+}
+
+getGiphy();
