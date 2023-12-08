088f28834cc2ed32265389e26a957ab557224092
RipNasty <det625@gmail.com>
2023-12-08 17:57:29 -0500

    final update

--- apiCall.js
+++ apiCall.js
@@ -1,17 +1,24 @@
 import fetch from 'node-fetch';
 
 async function fetchGiphyData() {
+    
+    // Define apiKey and apiUrl up top
     const apiKey = 'YJX6Vs4kayAeOM4etN7P5ueL4ie4wU5D';
     const apiUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`;
 
     try {
+        // Fetch the data
         const response = await fetch(apiUrl);
 
+        // Display error if fetch fails
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
+        // Convert response to JSON
         const data = await response.json();
+
+        // Destructure the response
         return data.data;
     } catch (error) {
         console.error('Fetch error:', error);
